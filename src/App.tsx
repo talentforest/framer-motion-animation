eff7444592b4cd98537a0be0f6c84e44b9306aac
talentforest <talentforest0501@gmail.com>
2022-04-14 22:53:12 +0900

    gestures

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,31 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.3,
-      // 모든 자식들을 딜레이시킬 수 있다.
-      staggerChildren: 0.1,
-      // 자식들 하나하나에 딜레이를 줘서 하나식 나타나게 만든다.
-    },
-  },
-};
-
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10,
-  },
-  end: {
-    opacity: 1,
-    y: 0,
-    // x, y는 css가 아니라 좌표를 수정하고 싶을 때 motion에서만 사용되는 속성!
-  },
+  hover: { scale: 1.5, rotateZ: 90 },
+  tap: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "#333", transition: { duration: 2 } },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null); // typescript에게 이 ref는 div요소라고 알려줌
   return (
     <Wrapper>
-      <Box variants={boxVariants} initial="start" animate="end">
-        {/* 부모의 애니메이션 객체는 자식에게 상속된다. */}
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-        <Circle variants={circleVariants} />
-      </Box>
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          variants={boxVariants}
+          whileHover="hover" // 마우스가 위에 있는 동안
+          whileTap="tap" // 마우스를 클릭하고(누르고) 있는 동안
+          drag // x: 가로선만 움직이도록 락 y: 세로선만 움직이도록 락
+          dragSnapToOrigin //다른 위치로 끌었더라도 마우스를 놓으면 다시 원위치로 오게 됨.
+          dragElastic={0.5} // 1은 그냥 밖으로 마우스 포인터를 따라감. 내려갈수록 마우스 포인터에 맞지 않게 끌려감. 0은 원래대로 부모요소 밖을 벗어나지 않음.
+          dragConstraints={biggerBoxRef} // 최대 어디까지 움직일 수 있는지 거리 조정해줌. 만약 부모요소 안에서만 움직여야 한다면 ref를 이용해 지정해준 다음 이 ref를 넣어주면 된다.
+          whileDrag="drag" // 마우스로 드래그하는 동안
+          // ✅ 색상값을 string이 아니라 숫자형으로 적어줘야 애니메이션이 잘 변화됨.
+          // "black" (x) "#000" (o)
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
@@ -55,23 +38,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const BiggerBox = styled(motion.div)`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
   background-color: #fff;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Circle = styled(motion.div)`
-  width: 70px;
-  height: 70px;
-  place-self: center;
-  background-color: #888;
-  border-radius: 35px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default App;

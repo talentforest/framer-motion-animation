70732d9c766aba7e7410dd29bf51b89f69b45f0a
talentforest <talentforest0501@gmail.com>
2022-04-14 17:25:30 +0900

    variant

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
-const myVars = {
-  start: { scale: 0 },
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
   end: {
     scale: 1,
-    rotateZ: 360,
+    opacity: 1,
     transition: {
       type: "spring",
-      damping: 10,
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.3,
+      // 모든 자식들을 딜레이시킬 수 있다.
+      staggerChildren: 0.1,
+      // 자식들 하나하나에 딜레이를 줘서 하나식 나타나게 만든다.
     },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+    // x, y는 css가 아니라 좌표를 수정하고 싶을 때 motion에서만 사용되는 속성!
   },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box
-        variants={myVars}
-        initial="start"
-        animate="end"
-        //variant는 일종의 애니메이션 스테이지,
-        //initial 초기상태부터 finish 종료상태까지 모든 속성을 넣을 수 있다.
-        //initial, animate(finish)에 객체 키값을 적어주면 된다.
-      />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* 부모의 애니메이션 객체는 자식에게 상속된다. */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
@@ -39,9 +58,20 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: #fff;
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  background-color: #888;
+  border-radius: 35px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 export default App;

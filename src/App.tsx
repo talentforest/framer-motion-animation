419801c7ce4f53c6bc24dc1455b56808f7ee092d
talentforest <talentforest0501@gmail.com>
2022-04-18 12:09:09 +0900

    AnimateSharedLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
+const overlay = {
+  hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggledClicked = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<null | string>(null);
 
   return (
-    <>
-      <Wrapper onClick={toggledClicked}>
-        <Box>
-          {!clicked ? (
-            <Circle layoutId="circle" style={{ borderRadius: "50px" }} />
-          ) : null}
-        </Box>
-        <Box>
-          {clicked ? (
-            <Circle layoutId="circle" style={{ scale: "1.2" }} />
-          ) : null}
-        </Box>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Grid>
+        {["1", "2", "3", "4"].map((id) => (
+          <Box onClick={() => setId(id)} key={id} layoutId={id} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            variants={overlay}
+            onClick={() => setId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
+    </Wrapper>
   );
 }
 
@@ -28,25 +39,36 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
 const Box = styled(motion.div)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 300px;
   height: 200px;
-  margin: 20px;
   border-radius: 50px;
   background-color: rgba(255, 255, 255, 1);
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  background-color: #00a5ff;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default App;
